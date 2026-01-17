eeb688b572d633d86053d93eca0110bac50b9bc5
BrunoAndrade-dev <brunorafha4@gmail.com>
2026-01-17 13:03:42 -0300

    Introdução a membros estaticos

---
+++ JavaFundamental/MembrosEstaticos/programvERSION1.java
@@ -0,0 +1,28 @@
+package MembrosEstaticos;
+import java.util.*;
+
+// VERSÃO 1 
+public class programvERSION1 {
+ 
+    public static void main (String[] args) { 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner (System.in) ; 
+
+        System.out.println("ENTER RADIUS : ");
+        double radius = sc.nextDouble() ;
+
+        double c = MembrosEstaticos.calculator.circumference(radius) ; 
+
+        double v = MembrosEstaticos.calculator.volume(radius) ; 
+
+        System.out.printf("Circunferencia : %.2f%n"  , c);
+
+        System.out.printf("Volume : %.2f%n", v);
+
+        System.out.printf("PI : %.2f%n" , MembrosEstaticos.calculator.PI);
+
+        sc.close() ; 
+    }
+
+}
+
